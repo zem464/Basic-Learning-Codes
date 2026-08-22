9447fed86012d79b4dc2eee5e595838ac16a200d
Iris Membrere <irismembrere@gmail.com>
2026-08-22 23:23:18 +0800

    Added a try-catch statement and Changed the files to be created fresh each time.

--- Python_with_txt_files/even_odd_in_txt.py
+++ Python_with_txt_files/even_odd_in_txt.py
@@ -5,14 +5,22 @@
 
 # Create a method to process the text files
 def integers():
-    # Create a text file named for the 20 integers, even integers, and odd integers
-    with open("numbers.txt") as numbers_input, open("even.txt", "a") as even_num, open("odd.txt", "a") as odd_num:
-        for line in numbers_input:  # Read the text line by line
-            if line.strip():        # Convert the line to an integer
-                int_input = int(line)
-                if int_input % 2 == 0:  # Check if line is even, if even, write to even.txt
-                    even_num.write(str(int_input) + "\n")
-                else:   # if odd, write to odd.txt
-                    odd_num.write(str(int_input) + "\n")
+    try:
+        # Create a text file named for the 20 integers, even integers, and odd integers
+        with open("numbers.txt", "r") as numbers_input, \
+            open("even.txt", "w") as even_num, \
+            open("odd.txt", "w") as odd_num:
+
+            for line in numbers_input:  # Read the text line by line
+                if line.strip():        # Convert the line to an integer
+                    int_input = int(line)
+                    if int_input % 2 == 0:  # Check if line is even, if even, write to even.txt
+                        even_num.write(str(int_input) + "\n")
+                    else:   # if odd, write to odd.txt
+                        odd_num.write(str(int_input) + "\n")
+
+    except FileNotFoundError:
+        print("The file numbers.txt does not exist. Please create the file and add 20 integers.")
+
 # End the Program
 integers()
